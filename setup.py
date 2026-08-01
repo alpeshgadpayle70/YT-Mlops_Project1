b652f1a5dee52eb796e5717ee4b2a58b65a1ead3
Alpesh <alpeshgadpayle70@gmail.com>
2026-08-01 14:11:00 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
+    packages=find_packages()
+)
+
+#This setup file is help to install local packages inside our environment
